b9ff7d246e4bc231559270f7478b3a8e1c8e98a4
Lychee80 <Lychee.776@gmail.com>
2023-01-10 19:58:33 -0800

    test

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -3,21 +3,31 @@ package com.nighthawk.spring_portfolio.mvc.calculator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+
 @RestController
 @RequestMapping("/api/calculator")
 public class CalculatorApiController {
-    @GetMapping("/calculate")
-    public ResponseEntity<String> calculate(@RequestBody final String expression) {
-        try {
-            Calculator calculatedExpression = new Calculator(expression);
-            return new ResponseEntity<>(calculatedExpression.toString(), HttpStatus.ACCEPTED);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Internal Error/Parsing Error, check your expression", HttpStatus.BAD_REQUEST);
-        }
-    }
+
+  /**
+   * GET isLeapYear endpoint
+   * ObjectMapper throws exceptions on bad JSON
+   * 
+   * @throws JsonProcessingException
+   * @throws JsonMappingException
+   */
+  @GetMapping("/{expression}")
+  public ResponseEntity<String> getCalculation(@PathVariable("expression") String expression)
+      throws JsonMappingException, JsonProcessingException {
+    Calculator calculation = new Calculator(expression);
+    String result = calculation.toString(); // this requires exception handling
+
+    return new ResponseEntity<String>(result, HttpStatus.OK); // JSON response, see ExceptionHandlerAdvice for throws
+  }
 }
